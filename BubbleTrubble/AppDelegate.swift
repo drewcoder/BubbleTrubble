4a7260b199cefd24e6c4b696c406ad688b200ad5
drewcoder <drewcoder@gmail.com>
2022-03-31 16:34:39 -0400

    Initial Commit

---
+++ BubbleTrubble/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BubbleTrubble
+//
+//  Created by Ritchie Andrews on 3/31/22.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
